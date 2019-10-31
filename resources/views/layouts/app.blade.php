1fcc28ea3862ad942624232e3c85c2b969f4fb17
ikramhasib007 <ikramhasib007@gmail.com>
2019-11-01 01:53:52 +0600

    laravel-demo create with react ui

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Demo - @yield('title')</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<nav>
+		<ul>
+			<li><a href="" title=""></a></li>
+		</ul>
+	</nav>
+	<div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
